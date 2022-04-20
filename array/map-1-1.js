d1f4ecabb8429d59f9e6dd925ae701f905429a01
Leandro Marques <leandromarques.n@gmail.com>
2022-04-20 11:53:13 -0300

    feat: adicionando recursos para o projeto
    Mudanças adicionadas :
    	modified:   array/map-1-1.js

--- array/map-1-1.js
+++ array/map-1-1.js
@@ -11,6 +11,12 @@ caso tenha um array de 6 elementos o resultado final so map será um array de 6 
 
 const nums = [1,2,3,4,5,]
 
+//Este exemplo funciona para que o map returne o dobro do elemento do array nums
+let resultado = nums.map(function(e){
+    return e * 2
+})
+console.log(resultado)
+
 //Lembrando um metodo que passamos para um Map ele tem que ter um retorno.
 //E lembrando em uma função do tipo arrow => Ela possui um returno implicito.
 
@@ -24,3 +30,9 @@ const triplo = e => e * 3
 const paraDinheiro  = e => `R$ ${parseFloat(e).toFixed(2).replace('.',',')}`
 
 //nocodigo acima temos uma constante que recebe uma função do tipo arrow e dentro do nosso template String temos uma função parceFloat para transformar o tipo em  float  temos tofixed(2) para colocar em 2 casas decimais e o  replace('.',',')para susbstituir o ponto por virgula
+
+//Estaremos utilisando está funções  encadeadas uma após a outra utilizando a função MAP
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(`Mostrando o resultado dos maps com valores trandformados.`)
+console.log(resultado)
